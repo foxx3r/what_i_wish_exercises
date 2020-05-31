40c2dfa8941bc3256b1b46adb128070fe84499b8
foxx3r <coluna123@protonmail.ch>
2020-05-31 06:39:38 -0300

    fixed syntax error

--- ghc_extensions/multi_way_if.hs
+++ ghc_extensions/multi_way_if.hs
@@ -2,7 +2,7 @@
 
 bmiTell :: Float -> Text
 bmiTell bmi = if
-    | bmi <+ 18.5 -> "Underweight."
+    | bmi <= 18.5 -> "Underweight."
     | bmi <= 25.0 -> "Average weight."
     | bmi <= 30.0 -> "Overweight."
     | otherwise   -> "Clinically overweight."
